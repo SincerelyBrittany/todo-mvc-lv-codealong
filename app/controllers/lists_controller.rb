a3dd6d552215fbe386d6c21431e194d318a6b5df
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-18 20:57:48 -0400

    sets up the form_for creating a new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,7 @@
 class ListsController < ApplicationController
 
     def index
+        @list = List.new
         @lists = List.all
     end 
 
@@ -8,4 +9,9 @@ class ListsController < ApplicationController
         @list = List.find(params[:id])
         #params is going to have all the data passed by the user - params is a hash
     end 
+
+    def create
+        raise params.inspect
+    end
+
 end
