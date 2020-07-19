0676afb283bbedcb51b38b64f8a577d50543a31c
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-19 15:18:29 -0400

    _items_li.html updates

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -6,7 +6,16 @@ module ItemsHelper
     def li_for_item(item)
         content_tag :li, item, :class => li_class_for_item(item) do
             #What goes here
+            yield
         end 
     end 
+
+#    def form_for_item_status(item)
+#         form_for([item.list, item]) do |f| 
+#             f.check_box :status, :class => "completed", :checked => item.complete? 
+#             f.submit
+            
+#         end
+#    end
 
 end
