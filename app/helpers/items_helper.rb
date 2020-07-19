f69177a3f20eecd179e593374a7aafc2b8bf3972
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-19 14:40:39 -0400

    _items_li.html updates

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,4 +2,11 @@ module ItemsHelper
     def li_class_for_item(item)
         "completed" if item.complete?
     end 
+
+    def li_for_item(item)
+        content_tag :li, item, :class => li_class_for_item(item) do
+            #What goes here
+        end 
+    end 
+
 end
