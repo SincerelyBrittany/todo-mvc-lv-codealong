55b300ad34dd4994ab303f2efb2ba3e750c2d2c3
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-02 13:20:04 -0400

    adds user using github credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,43 @@ class SessionsController < ApplicationController
 
     def create 
         #raise cookies.inspect
-        @user = User.find_by(username: params[:username])
-        #cookies[:username] = @user.email
-        if @user && @user.authenticate(params[:password])
-            # session[:user_id]
-            log_in(@user)
-            flash[:sucess] = "Welcome, #{@user.username}"
-            redirect_to lists_path
-        else 
-            flash[:danger] = "Improper credentials given"
-            redirect_to login_path
-        end 
+        # raise "stop".inspect
+            #request.env
+            #request.class
+            #request.env["omniauth.auth"]
+            #request.env["omniauth.auth"]["provider"]
+            #request.env["omniauth.auth"]["uid"]
+            #request.env["omniauth.auth"].keys
+            #request.env["omniauth.auth"]["credentials"].keys
+            #request.env["omniauth.auth"]["token"] - allows you to make request ot gihub on user behalf - create a repo on user behalf for example
+        if auth_hash = request.env["omniauth.auth"] 
+                #they loggedin omniauth
+                #raise auth_hash.inspect
+            oauth_username = request.env["omniauth.auth"]["info"]["nickname"]
+            if user = User.find_by(:username => oauth_username) #github
+                session[:user_id] = user.id
+                redirect_to lists_path
+            else 
+                #I know them but this is the first time they came to this website
+                # raise "NEW USER LOGGING IN VIA GITHUB".inspect
+                user = User.create(:name => oauth_username, :username => oauth_username,:password => SecureRandom.hex)
+                session[:user_id] = user.id
+                redirect_to lists_path
+            end 
+        else
+            #normal log in
+            @user = User.find_by(username: params[:username])
+            #cookies[:username] = @user.email
+            if @user && @user.authenticate(params[:password])
+                # session[:user_id]
+                log_in(@user)
+                flash[:sucess] = "Welcome, #{@user.username}"
+                redirect_to lists_path
+            else 
+                flash[:danger] = "Improper credentials given"
+                redirect_to login_path
+            end 
+        end
     end 
 
     def destroy 
