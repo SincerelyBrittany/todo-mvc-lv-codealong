17950eae71a64f130eee21d4b8aaf9dc97b16a27
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-01 22:37:54 -0400

    updates omniauth and resets keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
     provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-    # provider :github,'846f4dbdecbd76a3a709Y','0e543ca8edf6766c9ae49a7eff309b4926f40314'
+
   end
    
